f258086ad84f041cf550171f6ef1311af5e2b993
Fionna <e0006117@u.nus.edu>
2017-09-20 01:52:19 +0800

    Add files

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -4,36 +4,39 @@
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
+#include <CRCGenerator.h>
 
 #define STACK_SIZE    200
 
 /**
  * Should we create a data structure to store the data?
  */
+
+ /* Device ID */
 int ARM_ID = 0, GYRO_ID = 1, THIGH_ID = 2;
-int accelx_hand_value = 0;
-int accely_hand_value = 0;
-int accelz_hand_value = 0;
 
-int gyrox_value = 0;
-int gyroy_value = 0;
-int gyroz_value = 0;
+/* Packet code */
+int ACK = 0, NAK = 1, HELLO = 2, READ = 3, WRITE = 4, DATA_RESP = 5;
 
-int accelx_thigh_value = 0;
-int accely_thigh_value = 0;
-int accelz_thigh_value = 0;
+/* Sensor values */
+float accelx_hand_value = 0;
+float accely_hand_value = 0;
+float accelz_hand_value = 0;
 
-int ACK = 0;
-int NAK = 1;
-int HELLO = 2;
-int reply;
-int handshake_flag;
+float gyrox_value = 0;
+float gyroy_value = 0;
+float gyroz_value = 0;
 
-int data = 0;
+float accelx_thigh_value = 0;
+float accely_thigh_value = 0;
+float accelz_thigh_value = 0;
 
 SemaphoreHandle_t semaphore = xSemaphoreCreateBinary();
 
 void handshake() {
+  int reply;
+  int handshake_flag = 1;
+  
   while (handshake_flag == 1) {
     if (Serial.available()) {
       reply = Serial.read();
@@ -87,31 +90,23 @@ void getVoltage(void *p){
 void sendDataToRaspberryPi(void *p){
   static TickType_t xLastWakeTime = xTaskGetTickCount();
   const TickType_t xPeriod = 500;
-
+  
   for(;;){
-    
     if(xSemaphoreTake(semaphore, (TickType_t) portMAX_DELAY) == pdTRUE){
       xSemaphoreGive(semaphore);
-      /**
-       * For firmware:
-       * 1) Package all data into a single dtring
-       * 2) Send data to the RasberryPI
-       */
     }
     vTaskDelayUntil(&xLastWakeTime, xPeriod);
   }
 }
 
 void setup() {
-  // Report sate 9600
   Serial.begin(9600);
-  handshake_flag = 1;
+  handshake();
 }
 
 void loop() {
-  handshake();
-  xTaskCreate(readDataFromSensors, "readDataFromSensors", STACK_SIZE, (void * ) NULL, 3, NULL);
+  xTaskCreate(readDataFromSensors, "readDataFromSensors", STACK_SIZE, (void *) NULL, 3, NULL);
   xTaskCreate(getVoltage, "getVoltage", STACK_SIZE, (void *) NULL, 2, NULL);
-  xTaskCreate(sendDataToRaspberryPi, "sendDataToRaspberryPi", STACK_SIZE, (void * ) NULL, 1, NULL);
+  xTaskCreate(sendDataToRaspberryPi, "sendDataToRaspberryPi", STACK_SIZE, (void *) NULL, 1, NULL);
   vTaskStartScheduler();
 }
