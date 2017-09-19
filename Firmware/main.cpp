9da3c87b7c74fc3cb56695d0e5dcc48997aa8360
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2017-09-19 16:19:51 +0800

    Update main.cpp
    Code now works on the Arduino IDE instead of  of Eclipse

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <avr/io.h>
-#include <FreeRTOS.h>
+#include <Arduino_FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
@@ -25,52 +25,52 @@ int accelz_thigh_value = 0;
 
 SemaphoreHandle_t semaphore = xSemaphoreCreateBinary();
 
-void readDataFromSensors(void *p){
-	static TickType_t xLastWakeTime = xTaskGetTickCount();
-	const TickType_t xPeriod = 500;
-
-	for(;;){
-		if(xSemaphoreTake(semaphore, (TickType_t) portMAX_DELAY) == pdTRUE){
-			xSemaphoreGive(semaphore);
-
-			/**
-			 * 1) Read data from sensors
-			 * 2) Package the data
-			 * 3) Store the data
-			 */
-
-		}
-		vTaskDelayUntil(&xLastWakeTime, xPeriod);
-	}
-}
-
-void sendDataToRaspberryPi(void *p){
-	static TickType_t xLastWakeTime = xTaskGetTickCount();
-	const TickType_t xPeriod = 500;
-
-	for(;;){
-		if(xSemaphoreTake(semaphore, (TickType_t) portMAX_DELAY) == pdTRUE){
-			xSemaphoreGive(semaphore);
-			/**
-			 * 1) Send data to the RasberryPI
-			 */
-		}
-		vTaskDelayUntil(&xLastWakeTime, xPeriod);
-	}
-}
-
 void setup() {
-	// Report sate 9600
-	Serial.begin(115200);
+  // Report sate 9600
+  Serial.begin(115200);
 }
 
 void loop() {
-	// Task to read data from Sensors
-	xTaskCreate(readDataFromSensors, "readDataFromSensors", STACK_SIZE, (void * ) NULL, 2, NULL);
+  // Task to read data from Sensors
+  xTaskCreate(readDataFromSensors, "readDataFromSensors", STACK_SIZE, (void * ) NULL, 2, NULL);
 
-	// Task to send data to RaspberryPi
-	xTaskCreate(sendDataToRaspberryPi, "sendDataToRaspberryPi", STACK_SIZE, (void * ) NULL, 1, NULL);
+  // Task to send data to RaspberryPi
+  xTaskCreate(sendDataToRaspberryPi, "sendDataToRaspberryPi", STACK_SIZE, (void * ) NULL, 1, NULL);
 
-	// Start Scheduler
-	vTaskStartScheduler();
+  // Start Scheduler
+  vTaskStartScheduler();
+}
+
+void readDataFromSensors(void *p){
+  static TickType_t xLastWakeTime = xTaskGetTickCount();
+  const TickType_t xPeriod = 500;
+
+  for(;;){
+    if(xSemaphoreTake(semaphore, (TickType_t) portMAX_DELAY) == pdTRUE){
+      xSemaphoreGive(semaphore);
+
+      /**
+       * 1) Read data from sensors
+       * 2) Package the data
+       * 3) Store the data
+       */
+
+    }
+    vTaskDelayUntil(&xLastWakeTime, xPeriod);
+  }
+}
+
+void sendDataToRaspberryPi(void *p){
+  static TickType_t xLastWakeTime = xTaskGetTickCount();
+  const TickType_t xPeriod = 500;
+
+  for(;;){
+    if(xSemaphoreTake(semaphore, (TickType_t) portMAX_DELAY) == pdTRUE){
+      xSemaphoreGive(semaphore);
+      /**
+       * 1) Send data to the RasberryPI
+       */
+    }
+    vTaskDelayUntil(&xLastWakeTime, xPeriod);
+  }
 }
