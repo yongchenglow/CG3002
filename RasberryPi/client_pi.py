c5a4c0478e54e41fc72231d8257cd6f9f8f887ea
Fionna <fionna@Fionns-MacBook-Pro.local>
2017-09-26 11:16:00 +0800

    Update message data, tested with server

--- RasberryPi/client_pi.py
+++ RasberryPi/client_pi.py
@@ -1,21 +1,42 @@
 import socket
 import sys
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
+import time
+
+secret_key = b'leader daryl goh'
+actions = ['busdriver', 'frontback', 'jumping', 'jumpingjack', 'sidestep',
+           'squatturnclap', 'turnclap', 'wavehands', 'windowcleaner360',
+           'windowcleaning', 'logout  ']
 
 def read(port):
+        i = 0
         s = socket.socket()
-        host = '192.168.0.105'
+        host = '10.213.28.223'
         s.connect((host,port))
-        try:
-            print 'connection done'
-            s.send(b'Testing')
-            s.close()
-        except socket.error, msg:
-            sys.stderr.write('error %s'%msg[1])
-            s.close()
-            print 'close'
-            sys.exit(2)
+        print('connected')
+        time.sleep(4)
+        
+        while True:
+            data = str(i)
+            msg = '#' + actions[i] + '|' + data + '|' + data + '|' + data + '|' + data        
+            length = 16 - (len(msg) % 16);
+            msg += length * ' '
+            
+            iv = Random.new().read(AES.block_size)
+            cipher = AES.new(secret_key, AES.MODE_CBC, iv)                
+            encoded = base64.b64encode(iv + cipher.encrypt(msg))
+            s.send(encoded)
+            
+            i += 1
+            if (i == 11):
+                s.close()
+                sys.exit()
+            else:
+                time.sleep(1)
 
 if __name__ == '__main__':
     port = 58198
-    print 'hey, checking TCP socket'
+    print('Checking TCP socket')
     read(port)
