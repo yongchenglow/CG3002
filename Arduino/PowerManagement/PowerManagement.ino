8fca03285d090ff049993de70c33c205748ef068
Khong Yew <khongyew@gmail.com>
2017-10-13 17:17:47 +0800

    Add energy consumed
    Reworked the entire code:
    - Voltage and current is sampled and averaged.
    - Time duration of sampling is tracked using current_millis - previous_millis
    - Average power is computed using average voltage and current
    -Energy Consumed computation is now possible, using average power x time duration

--- Arduino/PowerManagement/PowerManagement.ino
+++ Arduino/PowerManagement/PowerManagement.ino
@@ -1,90 +1,85 @@
-/*
- 11-14-2013
- SparkFun Electronics 2013
- Shawn Hymel
+#include <Arduino.h>
 
- This code is public domain but you buy me a beer if you use this 
- and we meet someday (Beerware license).
+#define CURRENT_SENSOR_PIN A0
+#define VOLTAGE_PIN A1
+#define SHUNT_RESISTOR 0.1
+#define SAMPLE_SIZE 200
+#define SAMPLING_DELAY 2
 
- Based on: https://learn.sparkfun.com/tutorials/ina169-breakout-board-hookup-guide
-  
- Description:
+float voltage_sum;
+float current_sum;
 
- This sketch shows how to use the SparkFun INA169 Breakout
- Board. As current passes through the shunt resistor (Rs), a
- voltage is generated at the Vout pin. Use an analog read and
- some math to determine the current. The current value is
- displayed through the Serial Monitor.
+unsigned long prev_time;
+unsigned long curr_time;
+float time;
 
- Hardware connections:
+float energy_consumed;
 
- Uno Pin    INA169 Board    Function
+void setup(){
+	Serial.begin(38400);
 
- +5V        VCC             Power supply
- GND        GND             Ground
- A0         VOUT            Analog voltage measurement
-
- VIN+ and VIN- need to be connected inline with the positive
- DC power rail of a load (e.g. an Arduino, an LED, etc.).
-
- */
-
-// Constants
-const int CURRENT_SENSOR_PIN = A0;    // Input pin for measuring Vout
-const float RS = 0.1;                 // Shunt resistor value (in ohms)
-const int VOLTAGE_REF = 5;            // Reference voltage for analog read
-const int VOLTAGE_SENSOR_PIN = A1;
-
-// Global Variables
-float sensorValue;   // Variable to store value from analog read
-float current;       // Calculated current value
-float voltage;
-float power;
-
-void setup() {
-
-  // Initialize serial monitor
-  Serial.begin(9600);
-
+	energy_consumed = 0;
+	
+	prev_time = 0;
+	
+	voltage_sum = 0;
+	current_sum = 0;
 }
 
 void loop() {
-
-  // Read Raw values from the INA169 board and voltage divider
-  sensorValue = analogRead(CURRENT_SENSOR_PIN);
-  voltage = analogRead(VOLTAGE_SENSOR_PIN);
-
-  // Remap the ADC value into a voltage number (5V reference)
-  sensorValue = (sensorValue * VOLTAGE_REF) / 1023;
-  voltage = (voltage * VOLTAGE_REF) / 1023;
-  
-  //Voltage reading is obtained from divider.
-  //Multiply reading by 2 to get the true voltage value of the batteries
-  voltage = voltage * 2;
-
-  // Follow the equation given by the INA169 datasheet to
-  // determine the current flowing through RS. Assume RL = 10k
-  // Is = (Vout x 1k) / (RS x RL)
-  current = sensorValue / (10 * RS);
-
-  //Power = Voltage * Current
-  power = voltage * current;
-
-  // Output value (in amps) to the serial monitor to 3 decimal
-  // places
-  Serial.print(current, 3);
-  Serial.println(" A");
-  
-  Serial.print(voltage, 3);
-  Serial.println(" V");
-  
-  Serial.print(power, 3);
-  Serial.println(" W");
-  
-  Serial.println();
-
-  // Delay program for a few milliseconds
-  delay(500);
-
+	
+	unsigned long voltage_sample_sum = 0;
+	unsigned long current_sample_sum = 0;
+	
+	//Summing N samples; 1 sample per 2ms
+	for(int i=0; i<SAMPLE_SIZE; i++){
+		voltage_sample_sum = voltage_sample_sum + analogRead(VOLTAGE_PIN);
+		current_sample_sum = current_sample_sum + analogRead(CURRENT_SENSOR_PIN);
+		delay(SAMPLING_DELAY);
+	}
+	
+	//logging the timestamp and duration at the end of the sampling
+	curr_time = millis();
+	time = (curr_time - prev_time)/1000.0/60.0/60.0; //length of time for the N sampled data, in terms of hours
+	prev_time = curr_time;
+	
+	//divide sample sum by N to find average
+	//remap digital values to analog
+	float voltage = remap(voltage_sample_sum/SAMPLE_SIZE);
+	float current = remap(current_sample_sum/SAMPLE_SIZE);
+	
+	//Multiply reading by 2 to get the true voltage value of the batteries
+	//Since voltage reading is obtained from divider that halfs the volatage.
+	voltage = voltage *2.0; //----------------------------------------> send this over serial
+	
+	//Based on datasheet of current sensor
+	// Is = (Vout x 1k) / (RS x RL)
+	current = current/(10 * SHUNT_RESISTOR); //---------------------> send this over serial
+	
+	float power = voltage * current;
+	energy_consumed = energy_consumed + (power * time);
+	
+	
+	
+	
+	
+	
+	
+	// Printing to serial monitor
+	Serial.print(current, 4);
+	Serial.println(" A");
+	
+	Serial.print(voltage, 4);
+	Serial.println(" V");
+	
+	Serial.print(power, 4);
+	Serial.println(" W");
+	
+	Serial.print(energy_consumed, 6);
+	Serial.println(" Wh\n");
 }
 
+// remap values from analogRead from 0-1023 to 0v-5v
+float remap(float raw_value){
+	return raw_value * 5 / 1023.0;
+}
