9e6c5f6e02018c8b6c3a0ccf744861afeff4fb11
darylgqy <darylgqy@gmail.com>
2017-10-21 21:14:45 +0800

    Tidy some parts of training and real time code

--- Software/Machine Learning/CG3002_SVM(Validated)_V3.py
+++ Software/Machine Learning/CG3002_SVM(Validated)_V3.py
@@ -18,21 +18,13 @@ from numpy import array, zeros, argmin, inf
 from numpy.linalg import norm
 import time
 
+from ML_FUNCTIONS import time_features, segment_signal
 
 from multiprocessing import Process
 
-rawData = []
-arduinoFlag = True
 start = time.time()
-##### buffer #####
-bufFlag = 0
-if(time.time()-start == 20.0):
-    bufFlag = 1
-    start = time.time()     
-if(bufFlag == 1):               #begin preprocessing to predictive analysis
-    X = pd.DataFrame(rawData)
-
 ##### label encoder #####
+df = pd.read_csv('file:///C:/Users/Daryl/Desktop/CG3002_DANCE_DANCE/CG3002/Software/DanceDanceData/filtered_dance.csv') 
 y = pd.DataFrame(df['LABELS'])
 le = preprocessing.LabelEncoder()
 le.fit(df['LABELS'])
@@ -43,61 +35,11 @@ print (time.time()-start)
 X = np.array(df.drop(['LABELS'], 1)) #removing labels
 X = preprocessing.normalize(X) #normalize the dataset
 
-def segment_signal(df, window_size):
-    N = df.shape[0]
-    dim = df.shape[1]
-    K =int(N/window_size)
-    segments = np.empty((K, window_size, dim))
-    for i in range(K):
-        segment = df[i*window_size : i*window_size+window_size,:]
-        segments[i] = np.vstack(segment)
-    return segments
-
 segmented_df = segment_signal(X, 50)
 
-print (time.time()-start)
-nLayers = segmented_df.shape[0]
-nRows = segmented_df.shape[1]
-nColumns = segmented_df.shape[2]
 
-##### Mean of raw data #####
-mean_list = []
-for i in range(nLayers):
-    sliceLayer = segmented_df[i,::] 
-    row = []
-    for j in range(nColumns):
-        temp = sliceLayer[:,j] 
-        mean = np.mean(temp)
-        row = np.append(row, [mean])
-    mean_list = np.append(mean_list, row)
-mean_list = mean_list.reshape((nLayers, nColumns))
-
-##### std of raw data #####
-std_list = []
-for i in range(nLayers):
-    sliceLayer = segmented_df[i,::] 
-    row = []
-    for j in range(nColumns):
-        temp = sliceLayer[:,j] 
-        std = np.std(temp)
-        row = np.append(row, [std])
-    std_list = np.append(std_list, row)
-std_list = std_list.reshape((nLayers, nColumns))
-
-median_list = []
-for i in range(nLayers):
-    sliceLayer = segmented_df[i,::] 
-    row = []
-    for j in range(nColumns):
-        temp = sliceLayer[:,j] 
-        median = np.median(temp)
-        row = np.append(row, [median])
-    median_list = np.append(median_list, row)
-median_list = median_list.reshape((nLayers, nColumns))
-
-
-#####Feature List#####
-feature_list = np.hstack((mean_list, std_list, median_list))    
+time_feature_list = []       
+time_feature_list = time_features(segmented_df, time_feature_list)
 
 ##### labels #####
 y_list = []
@@ -121,7 +63,7 @@ for i in range(nLayers):
 y_list = np.floor(y_list)
 
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(feature_list,
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(time_feature_list,
                                                    y_list, test_size = 0.25)
 clf = SVC()
 clf.fit(X_train, y_train)
@@ -130,7 +72,7 @@ accuracy_rate_1 = clf.score(X_test, y_test)
 ##### Applying model to test set #####
 y_predict = clf.predict(X_test)
 ##### Average score achieved in validation K=10 #####
-validate_score = cross_val_score(clf, mean_list, y_list, cv= 10).mean()
+validate_score = cross_val_score(clf, time_feature_list, y_list, cv= 10).mean()
 ##### Confusion Matrix #####
 matrix = metrics.confusion_matrix(y_test, y_predict)
 
