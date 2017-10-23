8ed2b179fc6d2708d2e503dd70ca7586b6291694
darylgqy <darylgqy@gmail.com>
2017-10-23 15:53:09 +0800

    Update Consolidate_Script

--- Software/Machine Learning/Consolidate_Script.py
+++ Software/Machine Learning/Consolidate_Script.py
@@ -8,12 +8,22 @@ import pandas as pd
 import glob as glob
 
 
-
+from os import walk
 import os
-csv_list = []
-for root, dirs,files in os.walk("file:///C:/Users/Daryl/Desktop/busdriver", topdown=True):
-    for name in files:
-        csv_list.append(os.path.join(root, name))
-combined_csv = pd.concat([ pd.read_csv(f, header=None) for f in csv_list ], ignore_index=True)  
 
-combined_csv.to_csv('filtered_sit.csv')        
+df = pd.read_csv(r"file:///C:/Users/Daryl/Desktop/busdriver/busdriver.csv")
+print (df)
+f = []
+for (dirpath, dirnames, filenames) in walk(r"file:///C:/Users/Daryl/Desktop/busdriver"):
+    f.extend(filenames)
+    break
+print (f)
+csv_list = []
+for root, dirs,files in os.walk(r"file:///C:/Users/Daryl/Desktop/busdriver", topdown=True):
+    for name in files:
+        if name.endswith(".csv"):
+            csv_list.append(os.path.join(root, name))
+print (csv_list)
+'''combined_csv = pd.concat([ pd.read_csv(f, header=None) for f in csv_list ], ignore_index=True)  
+
+combined_csv.to_csv('filtered_sit.csv')   '''     
