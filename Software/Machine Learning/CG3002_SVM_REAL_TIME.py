3c7806a1d70c47e633c371e79c89ffc980a3fb75
darylgqy <darylgqy@gmail.com>
2017-10-23 15:52:35 +0800

    added arduino code and remove unused code

--- Software/Machine Learning/CG3002_SVM_REAL_TIME.py
+++ Software/Machine Learning/CG3002_SVM_REAL_TIME.py
@@ -16,13 +16,9 @@ from Crypto import Random
 from scipy import signal
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing, cross_validation, metrics
 from sklearn.svm import SVC
-from sklearn.preprocessing import label_binarize
-from sklearn.cross_validation import cross_val_score
-from sklearn.metrics import confusion_matrix
-from numpy import array, zeros, argmin, inf
-from numpy.linalg import norm
+from sklearn.externals import joblib
+from sklearn import preprocessing
 import time
 
 import ML_FUNCTIONS as ml
@@ -30,9 +26,10 @@ import ML_FUNCTIONS as ml
 from multiprocessing import Process
 
 rawData = []
+final_result = None
 arduinoFlag = True
-
 start = time.time()
+clf = joblib.load('trained_model.sav') 
 while (arduinoFlag == True):
     ##### buffer #####
     bufFlag = 0
@@ -55,29 +52,31 @@ while (arduinoFlag == True):
         
         time_feature_list = ml.time_features(X, time_feature_list) #feature extraction and conver to 2d
         
+       
+        ##### Predict #####
+        result = clf.predict(X)        
+        bufFlag = 0  
         
-      ''' X_train, X_test, y_train, y_test = cross_validation.train_test_split(feature_list,
-                                                           y_list, test_size = 0.25)
-        clf = SVC()
-        clf.fit(X_train, y_train)
-        accuracy_rate_1 = clf.score(X_test, y_test)
+def dataFromArduino():
+    global rawData
+    count = 0
+    flag = True
+    print('run dataFromArduino')
+    
+    while flag:
+        for i in range(0, 9):
+            raw = []
+            for j in range(0, 9):
+                raw.append(0)
+            rawData.append(raw)
         
-        ##### Applying model to test set #####
-        y_predict = clf.predict(X_test)
-        ##### Average score achieved in validation K=10 #####
-        validate_score = cross_val_score(clf, mean_list, y_list, cv= 10).mean()
-        ##### Confusion Matrix #####
-        matrix = metrics.confusion_matrix(y_test, y_predict)'''
+        print(rawData)
         
-        
-        results = pd.DataFrame(matrix, columns = ['busdriver', 'frontback', 'jumping', 'jumpingjack', 'sidestep', 'squatturnclap',\
-                                                  'turnclap', 'wavehands', 'window', 'window360'])
-            
-        results.rename(index ={0:'busdriver', 1:'frontback', 2:'jumping', 3:'jumpingjack', 4:'sidestep', 5:'squatturnclap',\
-                               6:'turnclap', 7:'wavehands', 8:'window', 9:'window360' }, inplace = True)
-        accuracy = pd.DataFrame([accuracy_rate_1],columns = ['ACCURACY'])
-        accuracy.rename(index ={0:'ACCURACY'}, inplace = True)
-        results = results.append(accuracy)
-        results = results.fillna('')
-        results.to_csv('Accuracy_Matrix_SVM.csv')
-        #print (time.time()-start)
+        if (count < 20):
+            count += 1
+        else:
+            flag = False
+            print('stop running dataFromArduino')
+
+Process(target=dataFromArduino, args=()).start()
+Process(target=learn, args=()).start()
