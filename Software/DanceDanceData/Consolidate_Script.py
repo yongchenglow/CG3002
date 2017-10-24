5d35833e5d9f7e2932469d07de5d92ec63779463
darylgqy <darylgqy@gmail.com>
2017-10-24 16:06:13 +0800

    Consolidate latest data and added labels

---
+++ Software/DanceDanceData/Consolidate_Script.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 01:10:35 2017
+
+@author: Daryl
+"""
+import pandas as pd
+import glob as glob
+
+
+from os import walk
+import os
+
+csv_list = []
+for root, dirs,files in os.walk(r"data231017", topdown=True):
+    for name in files:
+        if name.endswith(".csv"):
+            csv_list.append(os.path.join(root, name))
+print (csv_list)
+combined_csv = pd.concat([ pd.read_csv(f, header=None) for f in csv_list ], ignore_index=True)  
+
+combined_csv.to_csv('filtered_activities_23OCT2017.csv')   
--- Software/Machine Learning/Consolidate_Script.py
+++ Software/Machine Learning/Consolidate_Script.py
@@ -11,19 +11,12 @@ import glob as glob
 from os import walk
 import os
 
-df = pd.read_csv(r"file:///C:/Users/Daryl/Desktop/busdriver/busdriver.csv")
-print (df)
-f = []
-for (dirpath, dirnames, filenames) in walk(r"file:///C:/Users/Daryl/Desktop/busdriver"):
-    f.extend(filenames)
-    break
-print (f)
 csv_list = []
-for root, dirs,files in os.walk(r"file:///C:/Users/Daryl/Desktop/busdriver", topdown=True):
+for root, dirs,files in os.walk(r"file:///C:/Users/Daryl/Desktop/CG3002_DANCE_DANCE/CG3002/Software/Filtering/data231017", topdown=True):
     for name in files:
         if name.endswith(".csv"):
             csv_list.append(os.path.join(root, name))
 print (csv_list)
-'''combined_csv = pd.concat([ pd.read_csv(f, header=None) for f in csv_list ], ignore_index=True)  
+combined_csv = pd.concat([ pd.read_csv(f, header=None) for f in csv_list ], ignore_index=True)  
 
-combined_csv.to_csv('filtered_sit.csv')   '''     
+combined_csv.to_csv('filtered_activities_23OCT.csv')   
