ea4b4a1e11e778356ea0c91eec8449767fe15c5c
Fionna <fionna@Fionnas-MacBook-Pro.local>
2017-10-26 03:03:38 +0800

    Include server in test.py

--- Software/Machine Learning/test.py
+++ Software/Machine Learning/test.py
@@ -1,92 +1,106 @@
 import sys
-
-from multiprocessing import Process, Queue, Value, Manager
-from ctypes import c_char_p
 import time
-import pandas as pd
+import socket
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+
+from multiprocessing import Process, Queue, Manager
+from ctypes import c_char_p
 import numpy as np
 
-from scipy import signal
 from scipy import stats
-import pandas as pd
-import numpy as np
-from sklearn.svm import SVC
 from sklearn.externals import joblib
 from sklearn import preprocessing
 import ML_FUNCTIONS as ml
 
-def handshake(handshake_flag):
-    print('Checking handshake')
-    while (handshake_flag):
-        handshake_flag = False
-    print('Handshake completed')
-
-def learn(queue, action):
-    print('Running learn')
+def learn(queue):
     clf = joblib.load('trained_model.pkl') # load model
-    start = time.time()
-    learnflag = False
-    
-    while (time.time() - start < 3):
-        pass
-        
-    rawData = []
-    count = 0
-    while (count < 3000):
-        rawData.append(queue.get())
-        count += 1
-    learnflag = True
-    
-    #print(rawData)
+    i = 0
 
-    while (learnflag):
-        ##### buffer #####
-        bufFlag = 1
-        if(bufFlag == 1):               #begin preprocessing to predictive analysis
+    while (flags['logout'] == False):
+        if (flags['dataReady'] == True):
+            rawData = []
+            count = 0
+            while (count < 3000):
+                rawData.append(queue.get())
+                count += 1
+
             #X = np.array(rawData)
-            X = np.array(rawData, dtype=int, copy=True)
-            
+            X = np.array(rawData, dtype=int, copy=True) 
             X = preprocessing.normalize(X) #normalize the dataset
             X = ml.segment_signal(X, 50) #segmentation to 3d for feature extraction
             
             time_feature_list = []
             time_feature_list = ml.time_features(X, time_feature_list) #feature extraction and conver to 2d
-
+            
             ##### Predict #####
             result = clf.predict(time_feature_list)   
             result = stats.mode(result) #find the mode in result
             result = np.array(result[0])
             result = str(int(result))
-        
-        
+            
             result = ml.result_output(result) #output the result as string
-            bufFlag = 0  #reset flag to take in next dataset'''
             action.value = result
-            print(result)
-        learnflag = False
-    print('Stopping learn')
+            if (i == 20):
+                action.value = 'logout  '
+            i += 1
+            flags['mlReady'] = True
+
+def toServer(s):
+    secret_key = b'leader daryl goh'
+    i = 0
+    while (flags['logout'] == False):
+        if (flags['mlReady']):
+            msg = '#' + action.value + '|' + str(i) + '|' + str(i) + '|' + str(i) + '|' + str(i)    
+            length = 16 - (len(msg) % 16);
+            msg += length * ' '
+            print(msg)
+    
+            iv = Random.new().read(AES.block_size)
+            cipher = AES.new(secret_key, AES.MODE_CBC, iv)                
+            encoded = base64.b64encode(iv + cipher.encrypt(msg))
+            s.send(encoded)
+            
+            flags['mlReady'] = False
+            i += 1
+            if (action.value == 'logout  '):
+                flags['logout'] = True
 
 def dataFromArduino(queue):
-    print('Running dataFromArduino')
     count = 0
     
-    while (count < 3000):
+    while (flags['logout'] == False & count < 3000):
         queue.put([count, count, count, count, count, count, count, count, count])
         count += 1
-        
-    print('Stopping dataFromArduino')
+        if (count == 3000):
+            flags['dataReady'] = True
+            count = 0
+        else:
+            flags['dataReady'] = False
     
 if __name__ == '__main__':
-    queue = Queue() # Store real-time data from Arduino
+    queue = Queue()                         # Store real-time data from Arduino
+    s = socket.socket()
     
     manager = Manager()
-    action = manager.Value(c_char_p, "Hello")
+    action = manager.Value(c_char_p, "")    # Store action based on ML
+    flags = manager.dict({'logout' : False, 'dataReady' : False, 'mlReady' : False})
     
-    handshake(True)
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    s.connect((ip,port))
+    
     p1 = Process(target=dataFromArduino, args=(queue,))
-    p2 = Process(target=learn, args=(queue, action,))
+    p2 = Process(target=learn, args=(queue,))
+    p3 = Process(target=toServer, args=(s,))
     p1.start()
     p2.start()
-    p1.join()
+    p3.start()
+
     p2.join()
+    p3.join()
+    while (queue.empty() == False):
+        queue.get()
+    p1.join()
     sys.exit()
