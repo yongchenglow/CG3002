1931a3577835cd2c8d5445ca7abdc7dfaa90ce36
darylgqy <darylgqy@gmail.com>
2017-10-31 00:01:23 +0800

    added labels to halloweenData

--- Software/DanceDanceData/Consolidate_Script.py
+++ Software/DanceDanceData/Consolidate_Script.py
@@ -17,6 +17,6 @@ for root, dirs,files in os.walk(r"data231017", topdown=True):
         if name.endswith(".csv"):
             csv_list.append(os.path.join(root, name))
 print (csv_list)
-combined_csv = pd.concat([ pd.read_csv(f, header=None) for f in csv_list ], ignore_index=True)  
+combined_csv = pd.concat([ pd.read_csv(f, header=None) for f in csv_list ], ignore_index=True)
 
-combined_csv.to_csv('filtered_activities_23OCT2017.csv')   
+combined_csv.to_csv('filtered_activities_30OCT2017.csv')   
