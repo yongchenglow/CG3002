ec04d3ac8a0fa705427346be22d5d51bdd09a596
fionnatong <fionnatong@u.nus.edu>
2017-10-31 06:15:51 +0000

    Add main.py

--- Software/Machine Learning/main.py
+++ Software/Machine Learning/main.py
@@ -1,6 +1,8 @@
+s
 import sys
 import time
 import serial
+import queue
 
 import numpy as np
 import pickle
@@ -24,41 +26,37 @@ def handshake(handshake_flag):
             ser.write(ACK)                  # If true, change flag and ACK
             print('Handshake completed')
 
-def learn(buffer):  
+def learn(X):  
     print('learn')
     with open('my_trained_classifier.pkl', 'rb') as fid:
         clf = pickle.load(fid)
         
-        '''test = pd.read_csv("/home/pi/Desktop/CG3002/Software/DanceDanceData/data231017/turnclap/turnclap6.csv")
-        test = preprocessing.normalize(test)
-        test = ml.segment_signal(test, 50) #segmentation to 3d for feature extraction
-        feature_list = []
-        feature_list = ml.time_features(test,feature_list) #feature extraction and conver to 2d
+    '''test = pd.read_csv("/home/pi/Desktop/CG3002/Software/DanceDanceData/data231017/frontback/frontback5.csv")
+    test = preprocessing.normalize(test)
+    test = ml.segment_signal(test, 50) #segmentation to 3d for feature extraction
+    feature_list = []
+    feature_list = ml.time_features(test,feature_list) #feature extraction and conver to 2d
     
-        ##### Applying model to test set #####
-        result = clf.predict(feature_list)
+    ##### Applying model to test set #####
+    result = clf.predict(feature_list)
             
-        result = stats.mode(result) #find the mode in result
-        result = np.array(result[0])
-        result = str(int(result))
-        result = ml.result_output(result)
-        print(result)'''
-        X = np.array(buffer, dtype=int, copy=True) 
-        X = preprocessing.normalize(X) #normalize the dataset
-        X = ml.segment_signal(X, 50) #segmentation to 3d for feature extraction
-            
-        time_feature_list = []
-        time_feature_list = ml.time_features(X, time_feature_list) #feature extraction and conver to 2d
-            
-        ##### Predict #####
-        result = clf.predict(X)   
-        result = stats.mode(result) #find the mode in result
-        result = np.array(result[1])
-        result = str(int(result))
-            
-        result = ml.result_output(result) #output the result as string
-        print(result)
-        buffer.clear()
+    result = stats.mode(result) #find the mode in result
+    result = np.array(result[0])
+    result = str(int(result))
+    result = ml.result_output(result)
+    print(result)'''
+    
+    X = preprocessing.normalize(X) #normalize the dataset
+    X = ml.segment_signal(X, 50) #segmentation to 3d for feature extraction   
+    time_feature_list = []
+    time_feature_list = ml.time_features(X, time_feature_list) #feature extraction and conver to 2d
+    ##### Predict #####
+    result = clf.predict(time_feature_list)  
+    '''result = stats.mode(result) #find the mode in result
+    result = np.array(result[0])
+    result = str(int(result))   
+    result = ml.result_output(result) '''#output the result as string
+    print(result)
 
 def dataFromArduino():
     ACK = b"\x00"
@@ -88,10 +86,13 @@ def dataFromArduino():
     
     if (arduinoChecksum == checkSum):
         ser.write(ACK)                  # Send ACK to arduino if everything is received
-        buffer.append([dataList[2], dataList[3], dataList[4],
+        '''buffer.append([dataList[2], dataList[3], dataList[4],
                        dataList[6], dataList[7], dataList[8],
-                       dataList[10], dataList[11], dataList[12]])
-        print('Successful Transmission')
+                       dataList[10], dataList[11], dataList[12]])'''
+        queue.put([dataList[2], dataList[3], dataList[4],
+                   dataList[6], dataList[7], dataList[8],
+                   dataList[10], dataList[11], dataList[12]])
+        #print('Successful Transmission')
         sampleSize = 1
         cumVoltage = dataList[14]
         cumCurrent = dataList[15]
@@ -105,27 +106,33 @@ def dataFromArduino():
         cumPower = 0
         ser.write(NACK)                 # Send NACK if an kind of error occurs
     
-    return {'buffer': buffer, 'size' : sampleSize, 'cumVoltage': cumVoltage, 'cumCurrent': cumCurrent, 'cumPower': cumPower}
+    return {'buffer': queue, 'size' : sampleSize, 'cumVoltage': cumVoltage, 'cumCurrent': cumCurrent, 'cumPower': cumPower}
     
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyS0', 115200)
-    buffer = []
-    size = 0
-    cumulativeCurrent = 0
-    cumulativeVoltage = 0
-    cumulativePower = 0
-    energyConsumption = 0
-    totalTime = 0
+ser = serial.Serial('/dev/ttyS0', 115200)
+buffer = []
+queue = queue.Queue(3000)
+size = 0
+cumulativeCurrent = 0
+cumulativeVoltage = 0
+cumulativePower = 0
+energyConsumption = 0
+totalTime = 0
     
-    handshake(True)
-    while (True):
-        print(size)
-        results = dataFromArduino()
-        buffer.append(results['buffer'])
-        size += results['size']
-        cumulativeCurrent += results['cumCurrent']
-        cumulativeVoltage += results['cumVoltage']
-        cumulativePower += results['cumPower']
-        if (size == 3000):
-            learn(buffer)
-            size = 0
+handshake(True)
+while (True):
+    results = dataFromArduino()
+    queue = results['buffer']
+    size += results['size']
+    cumulativeCurrent += results['cumCurrent']
+    cumulativeVoltage += results['cumVoltage']
+    cumulativePower += results['cumPower']
+    print(size)
+    if (size == 3000):
+        rawData = []
+        while (size > 0):
+            rawData.append(queue.get())
+            size -= 1
+        X = np.array(rawData)
+        learn(X)
+        sys.exit(0)
+        #time.sleep(3)
